312d6f5de4ffca7637d04b231f3403731a0ad5fe
Marcelo Leite <marteloleite@gmail.com>
2022-04-21 21:39:44 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Container from './components/layout/Container'
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import Home from './components/pages/Home'
+import NewProject from './components/pages/NewProject'
+import Project from './components/pages/project'
+import Projects from './components/pages/Projects'
+function App() {
+ return (
+ <Router>
+ <Navbar />
+ <Container customClass="min-height">
+ <Routes>
+ <Route path="/" element={<Home />} />
+ <Route path="/projects" element={<Projects />} />
+ <Route path="/company" element={<Company />} />
+ <Route path="/contact" element={<Contact />} />
+ <Route path="/newproject" element={<NewProject />} />
+ <Route path="/project/:id" element={<Project />} />
+ </Routes>
+ </Container>
+ <Footer />
+ </Router>
+ );
+}
+export default App;
